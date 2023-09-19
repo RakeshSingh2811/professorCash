2f46771e6f95560f3dc377bea28e8d43f7558d43
RakeshSingh2811 <128033460+Rakesh-Es-dev@users.noreply.github.com>
2023-09-19 16:59:55 +0530

    csrf exempt

--- Teachers/views.py
+++ Teachers/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Topic, SupTopic, Subject
 from .form import VideoForm, SubjectForm, SupTopicForm
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def addTopic(request):
     form = VideoForm(request.POST or None, request.FILES or None)
     if form.is_valid():
@@ -14,6 +16,7 @@ def addTopic(request):
     return render(request, "Teachers/index.html", context)
 
 
+@csrf_exempt
 def addSupTopic(request):
     form = SupTopicForm(request.POST or None, request.FILES or None)
     if form.is_valid():
@@ -24,6 +27,7 @@ def addSupTopic(request):
     return render(request, "Teachers/index.html", context)
 
 
+@csrf_exempt
 def addSubject(request):
     form = SubjectForm(request.POST or None, request.FILES or None)
     if form.is_valid():
