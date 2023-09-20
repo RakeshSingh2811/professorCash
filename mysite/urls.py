98dd91bd2cf046c267bd8bdd02d1af0f4b3ce3b2
RakeshSingh2811 <128033460+Rakesh-Es-dev@users.noreply.github.com>
2023-09-20 17:22:43 +0530

    adding static path

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 urlpatterns = [
     path("", include("Teachers.urls")),
     path("admin/", admin.site.urls),
+    path('media/<path>', serve, {'document_root': settings.MEDIA_ROOT}),
+    path('static/<path>)$', serve, {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
