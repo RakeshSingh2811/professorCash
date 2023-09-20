b1cba0d8f77216f3fab40e77ad48c338e20c461a
RakeshSingh2811 <128033460+Rakesh-Es-dev@users.noreply.github.com>
2023-09-20 16:44:53 +0530

    adding static path

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,9 +12,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import sys
+
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
